5bce576527871557ce68d5127428abb24534bd06
Diego Baca <dbaca1025@gmail.com>
2018-01-22 14:08:17 -0500

    made folders

--- Supporting Files/AppDelegate.swift
+++ Supporting Files/AppDelegate.swift
@@ -16,7 +16,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // TODO:- customize tabbar buttons
+        let mapVC = MapViewController()
+        mapVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        let collectionsVC = CollectionsViewController()
+        collectionsVC.tabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 1)
+        
+        let navVC1 = UINavigationController(rootViewController: mapVC)
+        let navVC2 = UINavigationController(rootViewController: collectionsVC)
+        
+        let tabbarVC = UITabBarController()
+        tabbarVC.viewControllers = [navVC1, navVC2]
+        
+        
         window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = tabbarVC
+        window?.makeKeyAndVisible()
         return true
     }
 
