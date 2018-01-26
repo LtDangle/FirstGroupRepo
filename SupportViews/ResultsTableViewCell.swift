c5a83968374dd04e822a6f1c0a903c721c976632
Diego Baca <dbaca1025@gmail.com>
2018-01-26 12:44:07 -0500

    finished list of files

--- SupportViews/ResultsTableViewCell.swift
+++ SupportViews/ResultsTableViewCell.swift
@@ -1,32 +1,39 @@
 //
-//  ResultsTableViewCell.swift
+//  RestaurantTableViewCell.swift
 //  Unit5FirstGroupProject
 //
-//  Created by C4Q on 1/20/18.
+//  Created by C4Q on 1/19/18.
 //  Copyright © 2018 C4Q. All rights reserved.
 //
 
 import UIKit
+import SnapKit
 
 class ResultsTableViewCell: UITableViewCell {
     
     lazy var resultsImageView: UIImageView = {
         let iv = UIImageView()
+        iv.backgroundColor = .black
         return iv
     }()
     
-    lazy var restaurantNameLabel: UILabel = {
+    lazy var resultsLabel: UILabel = {
         let lab = UILabel()
+        //We can string interpolate the tags, name whatever here or I can separate it into two labels
+        lab.text  = """
+        KFC
+        Fried Food, Fast Food
+        """
+        lab.textAlignment = .center
+        lab.backgroundColor = UIColor.green
+        lab.textColor = .white
         return lab
     }()
     
-    lazy var tagNameLabel: UILabel = {
-        let lab = UILabel()
-        return lab
-    }()
+    
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        super.init(style: style, reuseIdentifier: "ResultsTVCell")
         commonInit()
     }
     
@@ -39,17 +46,43 @@ class ResultsTableViewCell: UITableViewCell {
     private func commonInit() {
         backgroundColor = .white
         setUpViews()
+        setUpResultsImageViewConstraints()
+        setUpResultsLabel()
         
     }
     
     private func setUpViews() {
-        
+        let views = [resultsImageView, resultsLabel] as [UIView]
+        views.forEach{ addSubview($0); ($0).translatesAutoresizingMaskIntoConstraints = false }
     }
+    
+    
+    private func setUpResultsImageViewConstraints() {
+        resultsImageView.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(snp.top)
+            make.right.equalTo(snp.right)
+            make.left.equalTo(snp.left)
+            make.bottom.equalTo(snp.bottom).offset(-90)
+        }
+    }
+    
+    private func setUpResultsLabel () {
+        resultsLabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(resultsImageView.snp.bottom)
+            make.height.equalTo(snp.height).multipliedBy(0.3)
+            make.right.equalTo(snp.right)
+            make.left.equalTo(snp.left)
+            make.bottom.equalTo(snp.bottom)
+            
+        }
+    }
+    
+    
+    
     
     public func configureCell() {
-        //This will take in the restaurant's name, type of restaurant, address etc.
-        //The cell with the address will segue to the directions
+        //This will take in the restaurant's name, tag, and restauranyt image
+        //The cell will segue to the restaurant detailed view
     }
-    
     
 }
